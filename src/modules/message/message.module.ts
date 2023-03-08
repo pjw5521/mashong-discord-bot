33c380e50aa21b1e849c31c343f3fb92970e0e47
LouiMinister <louiminister@gmail.com>
2023-03-08 13:13:17 +0900

    fix: 모듈 의존성으로 nest 빌드 안되는 버그 수정

--- src/modules/message/message.module.ts
+++ src/modules/message/message.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import MessageReplyFactory from './message-reply-factory';
 import { BarReply } from './replys/bar.reply';
 import { FooReply } from './replys/foo.reply';
+import Reply from "./replys/message-reply";
 
 @Module({
-    providers: [MessageReplyFactory, FooReply, BarReply],
+    providers: [MessageReplyFactory, Reply, FooReply, BarReply],
     exports: [MessageReplyFactory],
 })
 export class MessageModule {}
