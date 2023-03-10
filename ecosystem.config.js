2c352f3a1d8bd7cef4dd573a9badad28ae472a9f
LouiMinister <louiminister@gmail.com>
2023-03-11 02:01:34 +0900

    chore: pm2 config 설정

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apps: [
+        {
+            name: 'app',
+            // eslint-disable-next-line n/no-path-concat
+            script: `${__dirname}/dist/main.js`,
+        },
+    ],
+};
