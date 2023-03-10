424cf01abf57f3cfb56fe64798a57f343856238e
pjw5521 <hyunsang.youn@lge.com>
2023-03-10 11:29:26 +0900

    Feat : github api 연동 샘플 코드 추가

--- src/modules/interaction/replys/ping.reply.ts
+++ src/modules/interaction/replys/ping.reply.ts
@@ -1,15 +1,28 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { ChatInputCommandInteraction, Client } from 'discord.js';
 import { DISCORD_CLIENT } from 'src/constant/discord';
+import { Octokit } from '@octokit/rest';
 
 @Injectable()
 export class PingReply {
   static base = false;
   static command = 'ping';
   static description: 'discord Ping';
-  constructor(@Inject(DISCORD_CLIENT) private readonly client: Client) {}
+  octokit: Octokit;
 
-  send(interaction) {
-    interaction.reply(`${this.client.ws.ping}ms Pong!`);
+  constructor(@Inject(DISCORD_CLIENT) private readonly client: Client) { 
+    this.octokit = new Octokit();
+  }
+  
+  async send(interaction): Promise<any> {
+    
+    /* How to use -> change GET url, parameters */
+    const res = await this.octokit.request('GET /events', {
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28'
+      }
+    })
+
+    interaction.reply(`${this.client.ws.ping}` + res.data);
   }
 }
